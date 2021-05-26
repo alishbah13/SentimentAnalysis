9b5aef27172c428b866454bb58f807e28e75d538
alishbah13 <alishbah13@gmail.com>
2021-05-27 02:45:48 +0500

    helper functions 1.1

--- sc.py
+++ sc.py
@@ -9,7 +9,23 @@ import string
 
 
 data = pd.read_csv('Reviews1.csv')
-# print( data.head(1)['Text'] )
+# print( type( data.head()) )
+
+def get_pos_neg(dataset):
+    ## get all the positive and negative labelled reviews
+    pos = []
+    neg = []
+    for index, revs in dataset.iterrows():
+        if revs.Recommended == 1:
+            pos.append( revs['Text'] )
+        else:
+            neg.append( revs['Text']) 
+    # positives = 19314; negatives = 4172
+    return pos, neg  
+
+positive_reviews, negative_reviews = get_pos_neg( data ) 
+print(len(positive_reviews), " " , len(negative_reviews))
+
 
 def preprocess(revs):
     ## takes reviews and returns pre_processed reviews and dictionary of unique words
@@ -17,28 +33,30 @@ def preprocess(revs):
     stemmer = snowball.SnowballStemmer('english')
     stop_words = stopwords.words('english')
     dictionary = set()
-    for review in revs.iteritems():
-        print(review[0])
-        #name, values in df.iteritems():
+    for index, review in revs.iterrows():
         #1 tokenize 
-        review = tokenizer.tokenize(review[1])
+        review['Text'] = tokenizer.tokenize(review['Text'])
         #2 lowercase
-        review = [w.lower() for w in review]
+        review['Text'] = [w.lower() for w in review['Text']]
         #3 remove stopwords 
-        review = [wrd for wrd in review if not wrd in stop_words ]
+        review['Text'] = [wrd for wrd in review['Text'] if not wrd in stop_words ]
         #4 stem
-        review = [stemmer.stem(x) for x in review]
-        # print(review)
+        review['Text'] = [stemmer.stem(x) for x in review['Text']]
+        revs['Text'][index] = review['Text']
+        # print( revs['Text'][index] )
         # add unique words to dictionary
-        dictionary.update(review)
+        dictionary.update(review['Text'])
     
-    return list(dictionary), revs
-
-words, reviews = preprocess(data['Text'])
-
-# print(words)
-
-# print( reviews[1] )
+    return dictionary, revs
 
 
+words, data = preprocess(data)
+print( data.head())
 
+# The keys are a tuple (word, label) and 
+# the values are the corresponding frequency. 
+# The labels we'll use here are 1 for positive and 0 for negative.
+
+def make_count(dictionary, data):
+    
+    for index, row in data.iterrows():
