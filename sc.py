181c7758ccf57cd2c4399bb78edb0df746a5cfd0
Fakhiha Mirza <mirzafakhiha@gmail.com>
2021-05-29 07:57:18 +0500

    Predicting sentiment of a tweet

--- sc.py
+++ sc.py
@@ -1,3 +1,4 @@
+from nltk.probability import log_likelihood
 from nltk.sem.logic import TypeResolutionException
 import numpy as np
 import pandas as pd
@@ -64,13 +65,7 @@ def process_tweet(tweet):
     #3 remove stopwords 
     tweet_tokens= [stemmer.stem(wrd) for wrd in tweet_tokens if not wrd in stop_words ]
 
-    print(tweet_tokens)
     return tweet_tokens
-
-
-
-
-
 
 
 
@@ -93,92 +88,91 @@ def make_count(dictionary, data):
                     word_freq[ temp ] = 1
     return word_freq
 
-process_tweet("i am a happy larki")
+
+data = pd.read_csv('Reviews1.csv')
+# print( type( data.head()) )
+words, data = preprocess(data)
+#here words is a dict of unique words and data is processed tweet
+# positive_reviews, negative_reviews , Review_classifier = get_pos_neg( data ) 
+positive_reviews, negative_reviews = get_pos_neg( data ) 
+#^ here positive and negative reviews aare just list of strings and Review classifier has coresponding recommendations
+print(len(positive_reviews), " " , len(negative_reviews))
+
+# training set = 80% 
+# testing set = 20%
+# positives = 18540; negatives = 4101
+
+train_pos = positive_reviews[:14832]
+test_pos = positive_reviews[14832:]
+
+train_neg = negative_reviews[:3281]
+test_neg = negative_reviews[3281:]
+
+training_set = train_pos + train_neg
+test_set = test_pos + test_neg
+
+# print (len(Review_classifier))
+
+ratings = make_count(words, data)  # equi to freqs 
+
+# print(ratings[('silki', 1)])   # If you search manually silky gives 126, here it gives 111??
+# print(training_set)
+# print(test_set)
 
 
-# data = pd.read_csv('Reviews1.csv')
-# # print( type( data.head()) )
-# words, data = preprocess(data)
-# #here words is a dict of unique words and data is processed tweet
-# # positive_reviews, negative_reviews , Review_classifier = get_pos_neg( data ) 
-# positive_reviews, negative_reviews = get_pos_neg( data ) 
-# #^ here positive and negative reviews aare just list of strings and Review classifier has coresponding recommendations
-# print(len(positive_reviews), " " , len(negative_reviews))
+def training_naive_bayes(ratings, training_set):
+    loglikelihood = {}
+    logprior = 0
 
-# # training set = 80% 
-# # testing set = 20%
-# # positives = 18540; negatives = 4101
+    # calculate V, the number of unique words in the vocabulary
+    vocab = set([pair[0] for pair in ratings.keys()])
+    V = len(vocab)
 
-# train_pos = positive_reviews[:14832]
-# test_pos = positive_reviews[14832:]
+    # calculate N_pos and N_neg
+    N_pos = N_neg = 0
+    for pair in ratings.keys():
+        if pair[1] > 0:
+            N_pos += ratings[pair]
+        else:
+            N_neg += ratings[pair]
 
-# train_neg = negative_reviews[:3281]
-# test_neg = negative_reviews[3281:]
+    D = len(training_set)
+    D_pos = len(train_pos)
+    D_neg = D - D_pos
+    logprior = math.log(D_pos)  - math.log(D_neg)
+    # For each word in the vocabulary get pos and neg probabilitu
+    for word in vocab:
+        if (word,1.0) in ratings:
+            freq_pos = ratings[(word,1.0)]
+        else:
+            freq_pos = 0.0
+        if (word,0.0) in ratings:
+            freq_neg = ratings[(word,0.0)]
+        else :
+            freq_neg = 0.0
 
-# training_set = train_pos + train_neg
-# test_set = test_pos + test_neg
+        p_w_pos = (freq_pos +1 ) / (N_pos + V)
+        p_w_neg = (freq_neg +1 ) / (N_neg + V)
+        loglikelihood[word] = math.log(p_w_pos)  - math.log(p_w_neg)
 
-# # print (len(Review_classifier))
-
-# ratings = make_count(words, data)  # equi to freqs 
-
-# # print(ratings[('silki', 1)])   # If you search manually silky gives 126, here it gives 111??
-# # print(training_set)
-# # print(test_set)
-
-
-# def training_naive_bayes(ratings, training_set):
-#     loglikelihood = {}
-#     logprior = 0
-
-#     # calculate V, the number of unique words in the vocabulary
-#     vocab = set([pair[0] for pair in ratings.keys()])
-#     V = len(vocab)
-
-#     # calculate N_pos and N_neg
-#     N_pos = N_neg = 0
-#     for pair in ratings.keys():
-#         if pair[1] > 0:
-#             N_pos += ratings[pair]
-#         else:
-#             N_neg += ratings[pair]
-
-#     D = len(training_set)
-#     D_pos = len(train_pos)
-#     D_neg = D - D_pos
-#     logprior = math.log(D_pos)  - math.log(D_neg)
-#     # For each word in the vocabulary get pos and neg probabilitu
-#     for word in vocab:
-#         if (word,1.0) in ratings:
-#             freq_pos = ratings[(word,1.0)]
-#         else:
-#             freq_pos = 0.0
-#         if (word,0.0) in ratings:
-#             freq_neg = ratings[(word,0.0)]
-#         else :
-#             freq_neg = 0.0
-
-#         p_w_pos = (freq_pos +1 ) / (N_pos + V)
-#         p_w_neg = (freq_neg +1 ) / (N_neg + V)
-#         loglikelihood[word] = math.log(p_w_pos)  - math.log(p_w_neg)
-
-#     return logprior, loglikelihood
+    return logprior, loglikelihood
 
 
-# print("Yahan aagaya")
-# # print(len(training_set) ,"    " , len(positive_reviews))
-# # print(training_naive_bayes(ratings,training_set))
+print("Yahan aagaya")
+# print(len(training_set) ,"    " , len(positive_reviews))
+# print(training_naive_bayes(ratings,training_set))
 
 
-# def Prdeict_Tweet(tweet, logprior, loglikelihood):
+def Prdeict_Tweet(tweet, logprior, loglikelihood):
 
-#     Tweet_tokens = process_tweet(tweet)
-#     p = 0
-#     p += logprior
-#     for word in Tweet_tokens:
-#         if word in loglikelihood:
-#             p += loglikelihood[word]
-#     return p
+    Tweet_tokens = process_tweet(tweet)
+    p = 0
+    p += logprior
+    for word in Tweet_tokens:
+        if word in loglikelihood:
+            p += loglikelihood[word]
+    return p
 
 
-
+log_prior , log_likelihood = training_naive_bayes(ratings,training_set)
+print(Prdeict_Tweet("she smiled and was happy" , log_prior , log_likelihood ))
