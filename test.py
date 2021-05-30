f57e2de6ea723aa7412d8ea4e6e806dbd26976bd
Fakhiha Mirza <mirzafakhiha@gmail.com>
2021-05-31 00:27:10 +0500

    rating resolved 1.1

--- test.py
+++ test.py
@@ -8,9 +8,10 @@ f2 = open("range.txt" , "r")
 range = json.load(f2)
 f2.close()
 
-p = predict_review("good blouse will surely buy again " , probabilities['log_prior'] , probabilities['log_likelihood'])
+User_input = str(input("Enter your review : " ))
+p = predict_review(User_input , probabilities['log_prior'] , probabilities['log_likelihood'])
 
-rating = (5-0) * ((p - range["min"] ) / (range["max"] - range["min"] )) + 0
+rating = (5-1) * ((p - (range["min"] / 5) ) / (range["max"]/5 - range["min"]/5 )) + 1
 
 print("Prediction of review : " , p)
 print("Rating of review   :  ",format(rating, '0.4f'))
